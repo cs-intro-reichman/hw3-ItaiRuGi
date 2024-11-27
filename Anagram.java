398817ebc7e7c8f80b5f7a1b6df5b5f4217a2ba0
ItaiRuGi <itaipoplinger115@gmail.com>
2024-11-27 18:29:48 +0200

    Just 2/3

--- Anagram.java
+++ Anagram.java
@@ -6,11 +6,12 @@ public class Anagram {
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
-
+		System.out.println(isAnagram("null", "lolun"));//false
 		// Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
 		
 		// Tests the randomAnagram function.
+	
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
 		
 		// Performs a stress test of randomAnagram 
@@ -28,22 +29,79 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String base = preProcess(str1);
+		String check = preProcess(str2);
+		if(base.length() != check.length()){
+			return false;
+		}
+		for(int i = 0; i < base.length(); i++){
+			if(charof(base.charAt(i), check) == -1){
+				return false;
+			}else{
+				check = removefirstChar(base.charAt(i), check);
+			}
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String chng = "";
+		for(int i = 0; i < str.length(); i ++){
+			if((char)(str.charAt(i)) >= 65 && (char)(str.charAt(i)) <= 90){
+				chng += (char)(str.charAt(i) + 32);
+			}else if((char)(str.charAt(i)) >= 97 && (char)(str.charAt(i)) <= 122){
+				chng += (char)(str.charAt(i));
+			}
+		}
+		return chng;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
+		String newAnagram = "";
+		String sideString = str;
+		for(int i = 0; i < str.length(); i ++){
+			if(sideString.length() == 1){
+				newAnagram += sideString.charAt(0);
+				return newAnagram;
+			}else{
+			int idx = (int)(Math.random()*(sideString.length()));
+			newAnagram += sideString.charAt(idx);
+			String spare = "";
+			for(int j = 0; j < sideString.length(); j ++){
+				if(j != idx){
+					spare += sideString.charAt(j);
+				}
+			}
+			sideString = spare;
+			}
+		}
 		return "";
 	}
+	public static int charof(char let, String somet){ // returns the index of a given char in a string, or -1 if not in the string
+		for(int i = 0; i < somet.length(); i ++){
+			if(somet.charAt(i) == let){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static String removefirstChar(char rem, String inp){
+		boolean removed = false;
+		String ret = "";
+		for(int i = 0; i < inp.length(); i ++){
+			if(inp.charAt(i) != rem || (inp.charAt(i) == rem && removed == true)){
+				ret += inp.charAt(i);
+			}else if(inp.charAt(i) == rem && removed == false){
+				removed = true;
+			}
+		}
+		return ret;
+	}
 }
+
+	
